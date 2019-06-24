36d5374fdccb4038a32aef505239fec54025b8ac
Joseph Chingalo <profschingalo@gmail.com>
2019-06-24 15:43:02 +0300

    feat: Intiate data set config

---
+++ config/data-set-config.js
@@ -0,0 +1,9 @@
+const dataSetConfig = {
+    "xewWZMYbqYc": [],
+    "TfoI3vTGv1f": [],
+    "kSaoJVXNxZE": [],
+    "GzvLb3XVZbR": [],
+    "cap79mdf6Co": [],
+    "rm3y3VHPiFD": [],
+    "zeEp4Xu2GOm": []
+}
