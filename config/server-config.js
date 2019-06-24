1533ed0d1b1f6a180b101ee48d0a7ce5c43a0bdd
Joseph Chingalo <profschingalo@gmail.com>
2019-06-24 14:32:32 +0300

    chore(config files):

---
+++ config/server-config.js
@@ -0,0 +1,15 @@
+const sourceServer = {
+    url: "https://dhis.moh.go.tz",
+    username: "maintenance",
+    password: "Syst3mM@1nt3n@nc3"
+}
+const destinationServer = {
+    url: "https://dhis2tz.pathfinder.org",
+    username: "api_user",
+    password: "API_USER2019a"
+}
+
+module.exports = {
+    sourceServer,
+    destinationServer
+}
